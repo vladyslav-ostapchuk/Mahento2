1906c29712e25918c4a231f35db50f045642bb8a
”Vlad” <eustakio6412@gmail.com>
2022-12-13 15:20:48 +0200

    Task 1

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,10 @@
+"use strict"; module.exports = {
+
+  theme: {
+    area: 'frontend',
+    name: 'Vendor/theme',
+    locale: 'en_US',
+    files: ['css/styles-m', 'css/styles-l'],
+    dsl: 'less'
+  }
+}
