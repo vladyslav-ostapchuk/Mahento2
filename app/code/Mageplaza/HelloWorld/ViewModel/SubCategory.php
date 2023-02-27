7c7be37650bc2dcbcdf920ff4bf07f4e8fb2ff73
”Vlad” <eustakio6412@gmail.com>
2023-02-27 13:32:02 +0200

    15 delete newsletter and add only on HOME page and on https://magento.test/gallery/

--- app/code/Mageplaza/HelloWorld/ViewModel/SubCategory.php
+++ app/code/Mageplaza/HelloWorld/ViewModel/SubCategory.php
@@ -1,41 +1,51 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Mageplaza\HelloWorld\ViewModel;
 
-use \Magento\Catalog\Model\CategoryRepository;
+use Magento\Catalog\Model\Category;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
 
-class SubCategory extends \Magento\Framework\View\Element\Template implements \Magento\Framework\View\Element\Block\ArgumentInterface
+class SubCategory implements ArgumentInterface
 {
+    /**
+     * @var Category
+     */
+    protected $_registry;
 
+    /**
+     * @var CollectionFactory
+     */
+    protected $_categoryCollectionFactory;
+
+    /**
+     * @param \Magento\Framework\Registry $registry
+     * @param CollectionFactory $categoryCollectionFactory
+     */
     public function __construct(
-
-        \Magento\Framework\Registry $coreRegistry,
-        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        \Magento\Framework\Registry $registry,
+        CollectionFactory $categoryCollectionFactory
     ) {
-        $this->coreRegistry        = $coreRegistry;
-        $this->categoryFactory     = $categoryFactory;
+        $this->_registry = $registry;
+        $this->_categoryCollectionFactory = $categoryCollectionFactory;
     }
 
+    /**
+     * Retrieve child categories of the current category
+     *
+     * @return Category[]
+     */
     public function getCategories()
     {
+        $category = $this->_registry->registry('current_category');
+        if (!$category || !$category->getId()) {
+            return [];
+        }
 
-        $category = $this->getCurrentCategory();
-        if (!$category) return;
+        $collection = $this->_categoryCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+        $collection->addIdFilter($category->getChildren());
 
-        $categoryId = $category->getId();
-
-        $model = $this->categoryFactory->create();
-        $categories = $model->getCollection()
-            ->addAttributeToSelect(['name'])
-            ->addAttributeToFilter('parent_id', $categoryId)
-            ->addIsActiveFilter();
-
-        return $categories;
-    }
-    public function getCurrentCategory()
-    {
-        return $this->coreRegistry->registry('current_category');
+        return $collection->getItems();
     }
 }
